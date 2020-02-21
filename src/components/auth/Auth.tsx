9d34f836bbc04f1e4c2c06ac3a56787f86a7f3c5
ryan <user@superbook.com>
2020-02-21 16:07:49 -0500

    started building auth page and formfields

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -2,16 +2,21 @@ import React, { Component } from 'react'
 import { Pane } from 'evergreen-ui';
 import Login from '../auth/Login';
 import Signup from '../auth/Signup';
-
-
+import {
+    BrowserRouter as Router,
+  } from 'react-router-dom';
 
 export class Auth extends Component {
     render() {
         return (
            <Pane>
-               Auth Component
+               *Auth Component*
+               <div className = "login">
                <Login/>
+               </div>
+               <div className = "signup">
                <Signup/>
+               </div>
            </Pane>
         )
     }
--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
-import { Pane } from 'evergreen-ui';
+import './Login.css'
+import { Pane, FormField, Button, Label, TextInput } from 'evergreen-ui';
 
 export class Login extends Component {
     render() {
         return (
             <Pane>
-                Login
+                <FormField 
+                className = "Loginform"
+                border = "2px solid white"
+                >
+                    <h3>Welcome Back</h3>
+                <TextInput 
+                width = "20em" 
+                className = "logininputs" 
+                placeholder = "Email Address"
+                background= "tint3"
+                    // onchange={e=>this.setState({ value: e.target.value })}
+                    // value={state.value}
+                />
+                <br /><br />
+                <TextInput 
+                width = "20em" 
+                className = "logininputs" 
+                placeholder = "Password"
+                    // onchange={e=>this.setState({ value: e.target.value })}
+                    // value={state.value}
+                />
+                <br /><br />
+                </FormField>
             </Pane>
         )
     }
