0f20a1012cc47b665c988552512b2a267c98f28c
William Mckinney <williammckinney@Williams-MacBook-Pro.local>
2020-03-02 09:41:34 -0500

    updated store functionality for store and cart

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -18,6 +18,7 @@ interface ICartState{
     data: any;
     token: any;
     cartId: any;
+    cartitemId: any;
 }
 
 export class Cart extends React.Component <ICartProps, ICartState> {
@@ -33,13 +34,17 @@ export class Cart extends React.Component <ICartProps, ICartState> {
             data: [],
             token: localStorage.getItem('token'),
             cartId: -1,
+            cartitemId: 0,
         }
     }
 
     componentDidMount = () => {
         console.log(this.state.token);
         console.log(this.state.cartId);
+        this.grabCart();
+    }
 
+    grabCart = () => {
         this.state.token === null ? this.mapper(this.state.data) :
 
             fetch(`http://localhost:8000/cart/`, {
@@ -50,23 +55,53 @@ export class Cart extends React.Component <ICartProps, ICartState> {
                 })
         }).then(
             (res) => res.json()
-        ).then((cartdata) => {console.log(cartdata[0].id); this.setState({cartId: cartdata});
-            fetch(`http://localhost:8000/cartitem/${cartdata[0].id}`, {
-                method: 'GET',
-                headers: new Headers ({
-                    'Content-Type': 'application/json',
-                    'Authorization': this.state.token
-                })
-            }).then(
-                (res) => res.json()
-            ).then((data) => {this.mapper(data); console.log(data); this.setState({data: data})})
+        ).then((cartdata) => {console.log(cartdata); cartdata.length <= 0 ? this.mapper(this.state.data) : this.cartFetch(cartdata) ;this.setState({cartId: cartdata[0].id} )
     });
+    }
+
+    cartFetch = (cartdata: any) => {
+        fetch(`http://localhost:8000/cartitem/${cartdata[0].id}`, {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': this.state.token
+            })
+        }).then(
+            (res) => res.json()
+        ).then((data) => {data.sort(function(a:any, b:any){return a.id - b.id}); this.mapper(data); console.log(data); this.setState({data: data})})
     }
 
     mapper = (data: any) => {
         return data.length > 0 ? data.map((item: any, index: number) => {
-            return <CartDisplay name={item.name} description={item.description} price={item.price} quantity={item.quantity} weight={item.weight} onsale={item.onsale} key={index}/>
+            return <CartDisplay name={item.name} description={item.description} price={item.price} quantity={item.quantity} weight={item.weight} onsale={item.onsale} key={index} quantitymaker={this.quantitymaker} cartitemId={item.id} token={this.state.token} deleteme={this.deleteme}/>
         }) : <img src={Photo} style={{width: '100%', height: '100%'}}></img>
+    }
+
+    quantitymaker = (num : number, cartid: any) => {
+        fetch(`http://localhost:8000/cartitem/update/${cartid}`, {
+            method: 'PUT',
+            body: JSON.stringify({quantity: num}),
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': this.state.token
+            })
+        }).then(
+            (res) => res.json()
+        );
+        this.grabCart()
+    }
+
+    deleteme = (data: number) => {
+    fetch(`http://localhost:8000/cartitem/delete/${data}`, {
+            method: 'DELETE',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': this.state.token
+            })
+        }).then(
+            (res) => console.log(res.json())
+        );
+        this.grabCart()
     }
 
     render() {
